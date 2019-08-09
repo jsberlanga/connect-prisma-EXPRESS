eb0ab03bd2bc6dc55b968f41635ffbce06476059
Julio Soto <jsberlanga@gmail.com>
2019-08-09 19:28:12 +0200

    add req.user to every request

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { prisma } = require("../generated/prisma-client");
+const jwt = require("jsonwebtoken");
 
 const request = require("request");
 
@@ -14,8 +15,29 @@ router.use((req, res, next) => {
   next();
 });
 
+router.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const {
+      user: { id }
+    } = jwt.verify(token, process.env.JWTSECRET);
+    req.userId = id;
+  }
+  next();
+});
+
+router.use(async (req, res, next) => {
+  if (req.userId) {
+    const user = await prisma.user({ id: req.userId });
+    delete user.password;
+    req.user = user;
+  }
+  next();
+});
+
 router.get("/", (req, res, next) => {
-  if (!req.cookies.name) {
+  if (!req.user) {
     res.redirect("/login");
   } else {
     const nowPlayingUrl = `${moviedbUrl}now_playing${apiKey}`;
@@ -24,14 +46,14 @@ router.get("/", (req, res, next) => {
       res.render("index", {
         results: parsedResults.results.slice(0, 10),
         title: "Now Playing in Cinemas",
-        user: req.cookies.name
+        user: req.user.name
       });
     });
   }
 });
 
 router.get("/login", (req, res, next) => {
-  if (!req.cookies.name) {
+  if (!req.user) {
     res.render("login");
   } else {
     res.render("login", {
@@ -57,7 +79,12 @@ router.post("/process_login", async (req, res, next) => {
       loginError: "Ops! This combination user/password is incorrect."
     });
   } else {
-    res.cookie("name", name);
+    const expiresIn = 24 * 60 * 60;
+    const accessToken = jwt.sign({ user }, process.env.JWTSECRET, {
+      expiresIn: expiresIn
+    });
+
+    res.cookie("token", accessToken);
     res.redirect("/");
   }
 });
@@ -73,15 +100,20 @@ router.post("/process_register", async (req, res, next) => {
       userFound: "Ops! This user is already registered."
     });
   } else {
-    const newUser = await prisma.createUser({ name, password: hash });
+    const user = await prisma.createUser({ name, password: hash });
 
-    res.cookie("name", name);
+    const expiresIn = 24 * 60 * 60;
+    const accessToken = jwt.sign({ user }, process.env.JWTSECRET, {
+      expiresIn: expiresIn
+    });
+
+    res.cookie("token", accessToken);
     res.redirect("/");
   }
 });
 
 router.get("/logout", (req, res, next) => {
-  res.clearCookie("name");
+  res.clearCookie("token");
   res.redirect("/");
 });
 
